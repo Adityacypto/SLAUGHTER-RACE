36737415f96a59a8809f05634e516409324bf843
Adityacypto <84172029+Adityacypto@users.noreply.github.com>
2021-08-05 21:33:42 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,19 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+
+
+
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
+
+function logout(){
+       
+      localStorage.removeItem("username");
+      window.location= "index.html";
+}
